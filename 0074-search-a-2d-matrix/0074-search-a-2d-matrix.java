280626ef3ad377f977dd1c11de99b0866a600d48
teja degala <23p31a4465@acet.ac.in>
2026-07-02 11:01:17 +0530

    Time: 0 ms (100%), Space: 44 MB (24.12%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length,m=matrix[0].length;
+        for(int i=0;i<n;i++)
+        {
+            if(matrix[i][0]<=target  && matrix[i][m-1]>=target)
+            {
+                int l=0,r=m-1;
+                while(l<=r)
+                {
+                    int mid=l+(r-l)/2;
+                    if(matrix[i][mid]==target)
+                    {
+                        return true;
+                    }
+                    else if(target <matrix[i][mid])
+                    {
+                        r=mid-1;
+                    }
+                    else
+                    {
+                        l=mid+1;
+                    }
+                }
+            }
+        }
+        return false;
+
+       
+        
+    }
+    
+}
