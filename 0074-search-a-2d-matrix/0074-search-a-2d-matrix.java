6dee0d5ec1a79e606cca27758345798230801bff
teja degala <23p31a4465@acet.ac.in>
2026-07-03 10:59:59 +0530

    Time: 0 ms (100%), Space: 44.2 MB (8.5%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,3 +1,5 @@
+
+/* a much easier way and tc is also better (https://youtu.be/JXU4Akft7yk?si=BUDsz8wVkbdTCFfc) */
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n=matrix.length,m=matrix[0].length;
