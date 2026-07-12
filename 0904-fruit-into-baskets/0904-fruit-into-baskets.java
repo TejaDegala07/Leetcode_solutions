2101d63c4d0c20efb744d73da6b8e36f04bcd352
teja degala <23p31a4465@acet.ac.in>
2026-07-12 07:15:18 +0530

    Time: 54 ms (67.18%), Space: 70.5 MB (56.61%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -5,22 +5,18 @@ class Solution {
 
         while(r<fruits.length)
         {
-           
+           map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
                 while(map.size()>2)
                 {
                     map.put(fruits[l],map.get(fruits[l])-1);
                     if(map.get(fruits[l])==0) map.remove(fruits[l]);
                     l++;
                 }
-
                 
-            
-            
-                map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
 
-            if(map.size()<=2){
+            
                 ml=Math.max(ml,r-l+1);
-            }
+            
             r++;
             
         }
