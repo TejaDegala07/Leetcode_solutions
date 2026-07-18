91593b3313cde3fbedd046e662cb411553e91b24
teja degala <23p31a4465@acet.ac.in>
2026-07-18 22:45:36 +0530

    Time: 6 ms (18.9%), Space: 46 MB (7.89%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        Arrays.sort(nums);
+
+        int a=nums[0];
+        int b=nums[nums.length-1];
+
+
+        while(b!=0) {
+        int rem=a%b;
+        a=b;
+        b=rem;
+    }
+    return a;
+
+        
+    }
+}
