c42231ebc559765f75ba5464991e7c4874b2e1e6
teja degala <23p31a4465@acet.ac.in>
2026-07-20 08:41:26 +0530

    Time: 1 ms (100%), Space: 42.8 MB (0%) - LeetHub

--- 3993-maximum-value-of-an-alternating-sequence/3993-maximum-value-of-an-alternating-sequence.java
+++ 3993-maximum-value-of-an-alternating-sequence/3993-maximum-value-of-an-alternating-sequence.java
@@ -1,7 +1,8 @@
 class Solution {
     public long maximumValue(int n, int s1, int m) {
         if(n==1) return (long)s1;
-        long s=(long)s1+((long)(n/2)*m)-(n/2)+1;
+        long k=n/2;
+        long s=(long)s1+k*m-k+1;
         return s;
     }
 }
