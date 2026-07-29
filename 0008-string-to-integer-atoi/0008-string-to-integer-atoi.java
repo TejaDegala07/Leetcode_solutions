2e825b250f4c946cdf072d0b51d5c97bdc373457
teja degala <23p31a4465@acet.ac.in>
2026-07-29 20:43:19 +0530

    Time: 2 ms (11.27%), Space: 44 MB (24.55%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int myAtoi(String s) {
+        
+        char neg=' ';
+        long ans=0;
+        s=s.trim();
+        if(s.length()==0) return 0;
+        int dum=1;
+        int i=0;
+        if(s.charAt(i)=='-' || s.charAt(i)=='+') {
+            if(s.charAt(i)=='-'){
+            dum=-1;
+            }
+            else if(s.charAt(i)=='+'){
+                dum=1;
+            }
+            i++;
+        }
+
+        while(i < s.length() && Character.isDigit(s.charAt(i))){
+            ans*=10;
+            ans+=s.charAt(i)-'0';
+            if(dum==1 && ans>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if(dum==-1 && -ans<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            i++;
+        }
+
+
+        return (int) (dum*ans);
+    }
+}
