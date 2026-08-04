05cb7333709b55254868f207db44798f6409cc76
teja degala <23p31a4465@acet.ac.in>
2026-08-04 21:42:30 +0530

    Time: 3 ms (99.84%), Space: 46.2 MB (91.01%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values={1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1};
+        String[] sybl={"M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"};
+
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<values.length;i++){
+            while(num>=values[i]){
+                sb.append(sybl[i]);
+                num-=values[i];
+            }
+        }
+        return sb.toString();
+    }
+}
