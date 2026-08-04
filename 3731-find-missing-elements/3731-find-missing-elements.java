db0ef0f683151dcdfd78f29eec195ba75f1e9349
teja degala <23p31a4465@acet.ac.in>
2026-08-04 13:20:54 +0530

    Time: 5 ms (43.94%), Space: 46.6 MB (80.06%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,28 +1,18 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        Arrays.sort(nums);
-        int min=nums[0];
-        int max=nums[nums.length-1];
-        
-
-        for(int i=min;i<=max;i++)
-        {
-            Boolean flag=false;
-
-            for(int j=0;j<nums.length;j++)
-            {
-                if(nums[j]==i)
-                {
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag==false)
-            {
-                list.add(i);
-            }
+        List<Integer> list=new ArrayList<>();
+        TreeSet<Integer> ts=new TreeSet<>();
+        for(int i:nums){
+            ts.add(i);
         }
+        int min=ts.first();
+        int max=ts.last();
+        while(min<=max){
+            if(!ts.contains(min)) list.add(min);
+            min++;
+        }
+
+
 
         return list;
 
