62495a7177eb69b435381155e5fbe7d08b6f254f
teja degala <23p31a4465@acet.ac.in>
2026-08-05 11:56:32 +0530

    Time: 4 ms (99.03%), Space: 47.9 MB (12.08%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)==s.charAt(r)){
+                l++;
+                r--;
+            }
+            else{
+                return(chkpali(l+1,r,s) || chkpali(l,r-1,s));
+            }
+        }
+        return true;
+    }
+    public boolean chkpali(int i,int j,String s){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
