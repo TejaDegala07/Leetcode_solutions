d6b36619d60bac85329c22218d7c90ea7876ff2e
teja degala <23p31a4465@acet.ac.in>
2026-08-13 23:54:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+    int value=0,l=0,r=nums.length-1;
+     
+    while(l<r)
+    {
+        int mid=l=(r-l)/2;
+        if()
+    }
+    }
+}
