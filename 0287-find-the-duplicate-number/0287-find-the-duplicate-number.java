20233edfe1989178135f8429c82a1eb821e3eea1
teja degala <23p31a4465@acet.ac.in>
2026-08-27 19:15:09 +0530

    Time: 16 ms (39.28%), Space: 88 MB (23.65%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int i:nums){
+            if(!set.add(i)) return i;
+        }
+        return 0;
+    }
+}
