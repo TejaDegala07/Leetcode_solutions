47cbb20f539f6d401928681aab829460d37f7c6a
teja degala <23p31a4465@acet.ac.in>
2026-09-21 11:58:49 +0530

    Time: 4 ms (100%), Space: 47 MB (0%) - LeetHub

--- 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.java
+++ 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.java
@@ -3,10 +3,11 @@ class Solution {
         int cnt=0;
         for(int i=0;i<inter.length;i++){
             for(int j=i+1;j<inter.length;j++){
-                if(inter[i][0]>=inter[j][0] && inter[i][0]<=inter[j][1] ||
-                inter[i][1]>=inter[j][0] && inter[i][1]<=inter[j][1] ||
-                inter[j][0]>=inter[i][0] && inter[j][0]<=inter[i][1]||
-                inter[j][1]>=inter[i][0] && inter[j][1]<=inter[i][1]) cnt++;
+                // if(inter[i][0]>=inter[j][0] && inter[i][0]<=inter[j][1] ||
+                // inter[i][1]>=inter[j][0] && inter[i][1]<=inter[j][1] ||
+                // inter[j][0]>=inter[i][0] && inter[j][0]<=inter[i][1]||
+                // inter[j][1]>=inter[i][0] && inter[j][1]<=inter[i][1]) cnt++;
+                if(Math.max(inter[i][0],inter[j][0])<=Math.min(inter[i][1],inter[j][1]))cnt++;
             }
         }
         return cnt;
