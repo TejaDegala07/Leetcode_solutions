bcb2806a5eb8cf044ac8a314a95a5580749a97a4
teja degala <23p31a4465@acet.ac.in>
2026-09-21 11:56:18 +0530

    Time: 7 ms (0%), Space: 46.7 MB (0%) - LeetHub

---
+++ 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countIntersectingIntervals(int[][] inter) {
+        int cnt=0;
+        for(int i=0;i<inter.length;i++){
+            for(int j=i+1;j<inter.length;j++){
+                if(inter[i][0]>=inter[j][0] && inter[i][0]<=inter[j][1] ||
+                inter[i][1]>=inter[j][0] && inter[i][1]<=inter[j][1] ||
+                inter[j][0]>=inter[i][0] && inter[j][0]<=inter[i][1]||
+                inter[j][1]>=inter[i][0] && inter[j][1]<=inter[i][1]) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
